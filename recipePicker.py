69a950075945347511880661ebc0241abe295998
Ash <ashley.m.fletcher@outlook.com>
2023-09-26 02:16:12 +0100

    Fetch titles of recipes and pre-processed them to read properly

--- recipePicker.py
+++ recipePicker.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import ImageTk
 import sqlite3
+from numpy import random
 
 bg_colour = "#3d6466"
 
@@ -10,8 +11,22 @@ def fetch_db():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM sqlite_schema WHERE type='table'; ")
     all_tables = cursor.fetchall()
-    print(all_tables[0])
+
+    idx = random.randint(0, len(all_tables) - 1)
+
+    # fetch ingredients
+    table_name = all_tables[idx][1]
+    cursor.execute("SELECT * FROM " + table_name + ";")
+    table_records = cursor.fetchall()
+
     connection.close()
+    return table_name, table_records
+
+
+def pre_process(table_name, table_records):
+    title = table_name[:-6]
+    title = "".join([char if char.islower() else " " + char for char in title])
+    print(title)
 
 
 def load_frame1():
@@ -46,7 +61,8 @@ def load_frame1():
 
 
 def load_frame2():
-    fetch_db()
+    table_name, table_records = fetch_db()
+    pre_process(table_name, table_records)
 
 
 # initiallize app
@@ -61,6 +77,7 @@ for frame in (frame1, frame2):
     frame.grid(row=0, column=0)
 
 load_frame1()
+
 
 # run app
 root.mainloop()
