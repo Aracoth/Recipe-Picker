ed5d29e221c72b8ea25c2270765d7e77431467b2
Ash <ashley.m.fletcher@outlook.com>
2023-09-27 02:27:45 +0100

    formatted ingredients text and created second functioning button

--- recipePicker.py
+++ recipePicker.py
@@ -6,6 +6,11 @@ from numpy import random
 bg_colour = "#3d6466"
 
 
+def clear_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
 def fetch_db():
     connection = sqlite3.connect("recipes.db")
     cursor = connection.cursor()
@@ -26,10 +31,22 @@ def fetch_db():
 def pre_process(table_name, table_records):
     title = table_name[:-6]
     title = "".join([char if char.islower() else " " + char for char in title])
-    print(title)
+
+    ingredients = []
+
+    # ingredients
+    for i in table_records:
+        name = i[1]
+        qty = i[2]
+        unit = i[3]
+        ingredients.append(qty + " " + unit + " of " + name)
+
+    return title, ingredients
 
 
 def load_frame1():
+    clear_widgets(frame2)
+    frame1.tkraise()
     frame1.pack_propagate(False)
     # frame1 widgets
     logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
@@ -61,8 +78,46 @@ def load_frame1():
 
 
 def load_frame2():
+    clear_widgets(frame1)
+    frame2.tkraise()
+
     table_name, table_records = fetch_db()
-    pre_process(table_name, table_records)
+    title, ingredients = pre_process(table_name, table_records)
+
+    # frame1 widgets
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo_bottom.png")
+    logo_widget = tk.Label(frame2, image=logo_img, bg=bg_colour)
+    logo_widget.image = logo_img
+    logo_widget.pack(pady=20)
+
+    tk.Label(
+        frame2,
+        text=title,
+        bg=bg_colour,
+        fg="white",
+        font=("TkHeadingFont", 20),
+    ).pack(pady=25)
+
+    for i in ingredients:
+        tk.Label(
+            frame2,
+            text=i,
+            bg=bg_colour,
+            fg="white",
+            font=("TkMenuFont", 12),
+        ).pack()
+
+    tk.Button(
+        frame2,
+        text="BACK",
+        font=("TkHeadingFont", 18),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda: load_frame1(),
+    ).pack(pady=20)
 
 
 # initiallize app
@@ -74,7 +129,7 @@ frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
 frame2 = tk.Frame(root, bg=bg_colour)
 
 for frame in (frame1, frame2):
-    frame.grid(row=0, column=0)
+    frame.grid(row=0, column=0, sticky="nesw")
 
 load_frame1()
 
